3a3e65c0ce86673bc205c24bf24eeb74b549d9d3
indahpuspitap8-art <indahpuspitap8@gmail.com>
2026-02-02 06:35:27 +0700

    Finnnnaaalll

--- app/Views/layout/wrapper.php
+++ app/Views/layout/wrapper.php
@@ -68,7 +68,7 @@
   <!-- Footer -->
   <footer class="main-footer">
     <div class="pull-right hidden-xs"><b>Version</b> 1.0.0</div>
-    <strong>Copyright &copy; <?= date('Y') ?> <a href="#">Kantor Punggelan Tech</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; <?= date('Y') ?> <a href="#">Iqwana Indah Puspita</a>.</strong> All rights reserved.
   </footer>
 
 </div>
